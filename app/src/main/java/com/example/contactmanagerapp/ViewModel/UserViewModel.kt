a5cd7ddf8b387e2c65dbe1a47e7e2b3c0b96f5c8
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-10 16:42:10 +0000

    Connecting View Model With Repository

--- app/src/main/java/com/example/contactmanagerapp/ViewModel/UserViewModel.kt
+++ app/src/main/java/com/example/contactmanagerapp/ViewModel/UserViewModel.kt
@@ -5,8 +5,10 @@ import androidx.databinding.Bindable
 import androidx.databinding.Observable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.contactmanagerapp.room.User
 import com.example.contactmanagerapp.room.UserRepository
+import kotlinx.coroutines.launch
 
 class UserViewModel(private val repository : UserRepository) : ViewModel(), Observable {
 
@@ -15,10 +17,59 @@ class UserViewModel(private val repository : UserRepository) : ViewModel(), Obse
     private lateinit var userToUpdateOrDelete : User
 
     @Bindable
-    var inputName = MutableLiveData<String>()
+    var inputName = MutableLiveData<String?>()
 
     @Bindable
-    var inputEmail = MutableLiveData<String>()
+    var inputEmail = MutableLiveData<String?>()
 
+
+    @Bindable
+    val saveOrUpdateButtonText  = MutableLiveData<String>()
+
+    @Bindable
+    val clearAllOrDeleteButtonText = MutableLiveData<String>()
+
+    init {
+        saveOrUpdateButtonText.value="Save"
+        clearAllOrDeleteButtonText.value="Clear All"
+    }
+
+    fun saveOrUpdate(){
+        val name = inputName.value!!
+        val email = inputEmail.value!!
+
+        insert(User(0,name,email))
+
+        inputName.value = null
+        inputEmail.value = null
+    }
+
+    fun insert(user: User) = viewModelScope.launch {
+        repository.insert(user)
+    }
+
+    fun clearAllOrDelete(){
+        clearAll()
+    }
+
+    fun clearAll() = viewModelScope.launch {
+        repository.deleteAll()
+    }
+
+    fun update(user: User) = viewModelScope.launch {
+        repository.update(user)
+    }
+
+    fun delete(user: User) = viewModelScope.launch {
+        repository.delete(user)
+    }
+
+    override fun addOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
+    }
+
+    override fun removeOnPropertyChangedCallback(callback: Observable.OnPropertyChangedCallback?) {
+
+    }
 
 }
