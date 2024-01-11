cd4873fde5e7e887f72a0c178c02b84cb15dcae3
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-01-11 10:27:36 +0000

    Layout change and method updates for functionality
    Firstly add onclick and text binded with the data; changing the code of
    saveOrUpdate and of deleteOrClearAll and initUpdateAndDelete to ensure
    proper behavior;

--- app/src/main/java/com/example/contactmanagerapp/ViewModel/UserViewModel.kt
+++ app/src/main/java/com/example/contactmanagerapp/ViewModel/UserViewModel.kt
@@ -13,7 +13,7 @@ import kotlinx.coroutines.launch
 class UserViewModel(private val repository : UserRepository) : ViewModel(), Observable {
 
     val users = repository.users
-    private val isUpdateOrDelete = false
+    private var isUpdateOrDelete = false
     private lateinit var userToUpdateOrDelete : User
 
     @Bindable
@@ -35,13 +35,22 @@ class UserViewModel(private val repository : UserRepository) : ViewModel(), Obse
     }
 
     fun saveOrUpdate(){
-        val name = inputName.value!!
-        val email = inputEmail.value!!
+        if(isUpdateOrDelete){
+            //make update
+            userToUpdateOrDelete.name = inputName.value!!
+            userToUpdateOrDelete.email = inputEmail.value!!
 
-        insert(User(0,name,email))
+            update(userToUpdateOrDelete)
+        }else{
+            val name = inputName.value!!
+            val email = inputEmail.value!!
 
-        inputName.value = null
-        inputEmail.value = null
+            insert(User(0,name,email))
+
+            inputName.value = null
+            inputEmail.value = null
+        }
+
     }
 
     fun insert(user: User) = viewModelScope.launch {
@@ -49,7 +58,11 @@ class UserViewModel(private val repository : UserRepository) : ViewModel(), Obse
     }
 
     fun clearAllOrDelete(){
-        clearAll()
+        if(isUpdateOrDelete){
+            delete(userToUpdateOrDelete)
+        }else{
+            clearAll()
+        }
     }
 
     fun clearAll() = viewModelScope.launch {
@@ -58,15 +71,34 @@ class UserViewModel(private val repository : UserRepository) : ViewModel(), Obse
 
     fun update(user: User) = viewModelScope.launch {
         repository.update(user)
+
+        //resetting the Buttons and Fields
+        inputName.value = null
+        inputEmail.value = null
+        isUpdateOrDelete = false
+        saveOrUpdateButtonText.value="Save"
+        clearAllOrDeleteButtonText.value="Clear All"
     }
 
     fun delete(user: User) = viewModelScope.launch {
         repository.delete(user)
+
+        //resetting the Buttons and Fields
+        inputName.value = null
+        inputEmail.value = null
+        isUpdateOrDelete = false
+        saveOrUpdateButtonText.value="Save"
+        clearAllOrDeleteButtonText.value="Clear All"
     }
 
 
     fun initUpdateAndDelete(user: User){
-
+        inputName.value = user.name
+        inputEmail.value = user.email
+        isUpdateOrDelete = true
+        userToUpdateOrDelete = user
+        saveOrUpdateButtonText.value="Update"
+        clearAllOrDeleteButtonText.value="Delete"
     }
 
 
